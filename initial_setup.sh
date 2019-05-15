13568421a42f70d85f07c72aa729d6efba2e066d
vagrant <vagrant@vagrant.vm>
2019-05-15 14:59:09 +0000

    initial setup with ansible configuration

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install sshpass;
-sudo apt-get install ansible;
+sudo apt-get update && sudo apt-get install -y sshpass;
+sudo apt-get install -y ansible;
+cat << EOF > /etc/ansible/.ansible.cfg
+[default]
+host_key_checking = False
+EOF
