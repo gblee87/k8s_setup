cac55e438d5292020001035d02c7784e68a80f36
vagrant <vagrant@vagrant.vm>
2019-05-22 00:39:26 +0000

    20190522|gblee87|change ansible home dir to vagrant home(/home/vagrant/.ansible.cfg)

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get install -y sshpass;
 sudo apt-get install -y ansible;
-sudo cat>/etc/ansible/.ansible.cfg<<EOF
+sudo cat>~/.ansible.cfg<<EOF
 [defaults]
 host_key_checking = False
 EOF
