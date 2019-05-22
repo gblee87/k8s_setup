a79bb0bf0b6cab70e2a9492c176ea70d49e1d0f1
gblee87 <pechando@gmail.com>
2019-05-22 01:41:37 +0000

    update mis configured about ansible

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y sshpass;
-sudo apt-get install -y ansible;
-sudo cat>~/.ansible.cfg<<EOF
-[defaults]
-host_key_checking = False
-EOF
+#sudo apt-get update && sudo apt-get install -y sshpass;
+#sudo apt-get install -y ansible;
+#sudo cat>~/.ansible.cfg<<EOF
+#[defaults]
+#host_key_checking = False
+#EOF
+
+cd ./ansible
+
+ansible-playbook -i ./ansible/hosts ./ansible/playbook.yml
